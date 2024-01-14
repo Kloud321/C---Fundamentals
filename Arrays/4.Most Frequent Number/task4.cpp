89de8525aa74d7cae12c2c9f75ed954d9f269545
IvanD <damyanov.dimitrov.ivam@gmail.com>
2024-01-14 21:00:09 +0200

    Added solutions

--- Arrays/4.Most Frequent Number/task4.cpp
+++ Arrays/4.Most Frequent Number/task4.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
-#include <array>
 
 using namespace std;
 
 int main() {
-    int size, sum = 0, average;
+    int size;
     cin >> size;
 
     int arr[size];
 
     for (int i = 0; i < size; i++) {
         cin >> arr[i];
-        sum += arr[i];
     }
 
-    average = sum / size;
+    int mostFrequent = arr[0]; // Assume the first element is the most frequent
+    int maxFrequency = 1; // Initialize max frequency to 1 (at least one occurrence)
 
     for (int i = 0; i < size; i++) {
-        if (arr[i] >= average) {
-            cout << arr[i] << " ";
+        int currentFrequency = 1; // Frequency of the current element
+
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                currentFrequency++;
+            }
+        }
+
+        if (currentFrequency > maxFrequency) {
+            mostFrequent = arr[i];
+            maxFrequency = currentFrequency;
         }
     }
+
+    cout << mostFrequent << endl;
 
     return 0;
 }
